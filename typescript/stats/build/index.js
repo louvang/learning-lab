89eac3fe65809ac61e041b0af85fdc8f77090c25
Lou Vang <louvangdev@gmail.com>
2024-04-12 21:20:38 -0500

    Use enums, tuples, and generics to read and parse CSV file

--- typescript/stats/build/index.js
+++ typescript/stats/build/index.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log('hello world');
+Object.defineProperty(exports, "__esModule", { value: true });
+const MatchReader_1 = require("./MatchReader");
+const MatchResult_1 = require("./MatchResult");
+const reader = new MatchReader_1.MatchReader('football.csv');
+reader.read();
+let manUnitedWins = 0;
+for (let match of reader.data) {
+    if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResult_1.MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United won ${manUnitedWins} games.`);
