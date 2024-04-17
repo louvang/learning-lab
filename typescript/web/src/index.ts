44e954492d85f139da6310cad1ff18e791c71284
Lou Vang <louvangdev@gmail.com>
2024-04-16 22:18:59 -0500

    Add fetch and save methods to user class

--- typescript/web/src/index.ts
+++ typescript/web/src/index.ts
@@ -1,16 +1,5 @@
-import { User } from './model/User';
+import { User } from './models/User';
 
-const user = new User({ name: 'Jane', age: 27 });
+const user = new User({ name: 'new record', age: 0 });
 
-user.on('change', () => {
-  console.log('Change #1');
-});
-user.on('change', () => {
-  console.log('Change #2');
-});
-user.on('save', () => {
-  console.log('Save was triggered');
-});
-
-user.trigger('change');
-user.trigger('save');
+user.save();
