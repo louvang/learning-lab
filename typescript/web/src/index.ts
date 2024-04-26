cf89543286bf7b384b5c5234790b71df63742a45
Lou Vang <louvangdev@gmail.com>
2024-04-25 21:52:34 -0500

    Create UserForm with template and render methods

--- typescript/web/src/index.ts
+++ typescript/web/src/index.ts
@@ -1,9 +1,5 @@
-import { User } from './models/User';
+import { UserForm } from './views/UserForm';
 
-const collection = User.buildUserCollection();
+const userForm = new UserForm(document.getElementById('root') as Element);
 
-collection.on('change', () => {
-  console.log(collection);
-});
-
-collection.fetch();
+userForm.render();
