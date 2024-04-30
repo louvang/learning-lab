243f75c6f18202cec56d539adb6cbc528dfe8d0e
Lou Vang <louvangdev@gmail.com>
2024-04-29 20:04:59 -0500

    Add login routes

--- typescript/server/build/index.js
+++ typescript/server/build/index.js
@@ -4,14 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const loginRoutes_1 = require("./routes/loginRoutes");
+const body_parser_1 = __importDefault(require("body-parser"));
+const cookie_session_1 = __importDefault(require("cookie-session"));
 const app = (0, express_1.default)();
-app.get('/', (req, res) => {
-    res.send(`
-    <div>
-      <h1>Hi there</h1>
-    </div>
-  `);
-});
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use((0, cookie_session_1.default)({ keys: ['aljowiej903qeqr3'] }));
+app.use(loginRoutes_1.router);
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
---
+++ typescript/server/build/routes/loginRoutes.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.router = void 0;
+const express_1 = require("express");
+const router = (0, express_1.Router)();
+exports.router = router;
+router.get('/', (req, res) => {
+    res.send('hello');
+});
+router.get('/login', (req, res) => {
+    res.send(`
+    <form method="POST">
+      <div>
+        <label>Email</label>
+        <input name="email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input name="password" type="password" />
+      </div>
+      <button>Submit</button>
+    </form>
+  `);
+});
+router.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    if (email && password && email === 'hi@hi.com' && password === 'password') {
+        req.session = { loggedIn: true };
+        res.redirect('/');
+    }
+    else {
+        res.send('Invalid email or password');
+    }
+});
