ac8d4ea5766917d97efb46dcc14a53132083559e
Lou Vang <louvangdev@gmail.com>
2024-04-30 20:52:50 -0500

    Implement state in class-based component called App

--- typescript/rrts/src/index.tsx
+++ typescript/rrts/src/index.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el!);
+
 interface AppProps {
   color?: string;
 }
 
-const el = document.getElementById('root');
-const root = ReactDOM.createRoot(el!);
-
 class App extends React.Component<AppProps> {
+  state = { counter: 0 };
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
   render() {
-    return <div>{this.props.color}</div>;
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
   }
 }
 
